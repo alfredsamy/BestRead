320c741b2d9d9e662e7358a5c2205743fc60f867
Alfred Samy <Alfred Samy>
2016-12-07 00:04:12 +0200

    test init

---
+++ app/src/test/java/com.bestread.app.bestread/test1.java
@@ -0,0 +1,15 @@
+package com.bestread.app.bestread;
+
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class test1 {
+    @Test
+    public void testdemo() {
+        assertEquals("NOOOOOOO", "a", "a");
+    }
+
+    
+}
+
